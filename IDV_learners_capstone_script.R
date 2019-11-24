e8e616aa8f1f8bc5868031b62612dfae8a82ec7b
Denis Korolski <54414784+DKorolski@users.noreply.github.com>
2019-11-24 16:22:42 +0300

    Add files via upload

--- IDV_learners_capstone_script.R
+++ IDV_learners_capstone_script.R
@@ -5,7 +5,22 @@ if(!require(data.table)) install.packages ("data.table", repos = "http://cran.us
 if(!require(car)) install.packages("car", repos = "http://cran.us.r-project.org")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
 if(!require(readxl)) install.packages("readxl", repos = "http://cran.us.r-project.org") #Loading dataset
-z <- read_excel (path = 'cancer_1.xlsx', sheet = 'cancer')
+
+#Loading dataset
+library(rvest)
+url <- "http://tunedit.org/repo/DASL/CancerSurvival.arff"
+web <- read_html(url)
+t <- html_nodes(web,"textarea")
+desc <- html_text(t)
+desc1 <- str_sub(desc,-852,-4)
+desc5 <- gsub(",","\t",desc1)
+desc6 <- str_replace_all(desc5,"'","")
+desc7 <- read_tsv(desc6,col_names = c("Survival", "Organ"))
+z <- data.frame(desc7)
+str(z)
+
+# alternative loading from file (github repository link is provided) if url is broken z <- read_excel (path = 'cancer_1.xlsx', sheet = 'cancer')
+
 library (lubridate)
 #Preparing dataset
 #testing for any N/A
@@ -22,18 +37,18 @@ table(z)
 #dataset is small. Data is set of independent medical cases
 #Plotting dataset distibution
 boxgraph <- ggplot(data = z, aes(x = Organ, y = Survival)) +
-  geom_boxplot() + 
-  labs(x = "Diagnosis", y = "Survival time in days") +
-  theme(axis.text.x = element_text(angle = 15, vjust = 0.9, hjust = 0.5))
+geom_boxplot() + 
+labs(x = "Diagnosis", y = "Survival time in days") +
+theme(axis.text.x = element_text(angle = 15, vjust = 0.9, hjust = 0.5))
 #The plotted boxplot shows the presence of extremely high survival rates in each group. 
 #Since these outliers are uniformly distributed across all groups, it is more likely to conclude that these are not sample artifacts, but a strong sign of an asymmetric distribution. 
 #Their exclusion will lead to a distortion of the initial nature of the distribution, so it was decided to leave all the data in the array for further analysis.
 
 qq.raw <- ggplot(aes(sample = Survival), data = z) +
-  geom_qq() + geom_qq_line() +
-  scale_x_continuous(labels = NULL, name = "Theoretic quantiles") +
-  scale_y_continuous(labels = NULL, name = "Experimental quantiles") +
-  facet_wrap(~Organ, ncol = 5)
+geom_qq() + geom_qq_line() +
+scale_x_continuous(labels = NULL, name = "Theoretic quantiles") +
+scale_y_continuous(labels = NULL, name = "Experimental quantiles") +
+facet_wrap(~Organ, ncol = 5)
 
 #Performing t-test comparing selected organ (breast) with others
 Stomach <- (z$Survival[z$Organ == "Stomach"])
@@ -87,9 +102,11 @@ length(p)
 nrow(test)
 length(error)
 # Calculate RMSE
-sqrt(mean(error^2))
+rmse1<-sqrt(mean(error^2))
+rmse_result <- data_frame(method = "Linear regression model 1", RMSE = rmse1)
+rmse_result %>% knitr::kable()
 
-#CROSS-VALIDATION
+#CROSS-VALIDATION.Fit Predictive Models Over Different Tuning Parameters
 # Fit lm model using 10-fold CV: model
 model <- train(
   Survival~Organ , 
@@ -101,7 +118,10 @@ model <- train(
     verboseIter = TRUE
   )
 )
-model
+rmse2<-model$results$RMSE
+rmse_result <- data_frame(method = "Linear regression+ 10 fold cross validation", RMSE = rmse2)
+rmse_result %>% knitr::kable()
+
 # Fit lm model using 5 x 5-fold CV: model
 model <- train(
   Survival~Organ, 
@@ -114,7 +134,10 @@ model <- train(
     verboseIter = TRUE
   )
 )
-model
+rmse3<-model$results$RMSE
+rmse_result <- data_frame(method = "Linear regression+ 5x5 fold repeated cross validation", RMSE = rmse3)
+rmse_result %>% knitr::kable()
+
 # Show the coefficients
 mod <- lm(Survival~Organ, z)
 coef(mod)
@@ -140,11 +163,25 @@ survival_avgs <- train %>%
 predicted_ratings_bi <- mu + test %>%
   left_join(survival_avgs, by = "Organ") %>%
   .$b_i
-rmse_1 <- RMSE(test$Survival, predicted_ratings_bi)
-rmse_1
-#Checking for any flaws
-#install.packages("lintr")
-library(lintr)
-error <- lintr::lint("IDV_learners_capstone_script.R")
-print(error)
+rmse_4 <- RMSE(test$Survival, predicted_ratings_bi)
+rmse_result <- data_frame(method = "Linear regression+ regular model", RMSE = rmse_4)
+rmse_result %>% knitr::kable()
+#Checking for any flaws.Significant flaws solved with lintr.
+
+# Results
+
+rmse_result <- data_frame(method = "Linear regression model 1", RMSE = rmse1)
+
+rmse_results <- bind_rows(rmse_result,
+                          data_frame(method = "Linear regression+ 10 fold cross validation", RMSE = rmse2))
+rmse_results <- bind_rows(rmse_results,
+                           data_frame(method = "Linear regression+ 5x5 fold repeated cross validation", RMSE = rmse3))
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method = "Linear regression+ regular model", RMSE = rmse_4))
+rmse_results %>% knitr::kable()
+
+
 #Conclusion.This IDV project was examined  to observe data, check hypothesis and to predict survival time. The model evaluation performance through the RMSE ( root mean squared error) showed that the Linear regression models are useful to predict survival time on the test set. 
+
+print("Operating System:")
+version
